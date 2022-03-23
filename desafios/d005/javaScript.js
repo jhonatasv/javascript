7e12b5b8c36fe726447e75a6622e48fa8014967b
jhonatasv <jhonatasvieira42@gmail.com>
2022-03-23 18:35:56 -0300

    calculo hectometros feito

--- desafios/d005/javaScript.js
+++ desafios/d005/javaScript.js
@@ -1,26 +1,35 @@
 function clicou() {
   let distancia = Number(prompt('Digite uma distância em metro (m)'))
 
-  let nome = document.querySelector('#texto')
+  let paragraph= document.querySelector('#texto')
 
-  let texto = nome.textContent = `A distância de ${distancia} metros, corresponde a...`
+  let novo_Texto = paragraph.textContent = `A distância de ${distancia} metros, corresponde a...`
 
-  //calculando a distancia por (Km)
+  //calculando a quilometros (Km)
 
   let calculo_quilometros = `${distancia / 1000}`
   let arredonda_km = calculo_quilometros.substring(0, 5) // O método substr()retorna uma parte da string O especificado no índice.
   document.querySelector('.km').textContent = `${arredonda_km} quilômetros(Km)`
 
-  let calculo_hectometros = `${distancia / 100}`
-  // let arredonda_Hm = calculo_hectometros   ??
-  document.querySelector('.hm').textContent = `${calculo_hectometros} hectômetros (Hm)`
-                                                      
+  //calculando a hectômetros (Hm)
+
+  let calculo_hectometros = Number(`${distancia / 100}`)
+  let arredonda_Hm = calculo_hectometros.toFixed(3)      //O método toFixed() retorna uma string correspondente ao número usando, arredondando o número para a quantidade de casas decimais especificadas.
+  document.querySelector('.hm').textContent = `${arredonda_Hm} hectômetros (Hm)`
+
+  //calculando O decâmetros (Dam)
+
   let calculo_decametros = `${distancia * 10}`
-  // let arredonda_Dam = calculo_decametros
+  let arredonda_Dam = calculo_decametros
   document.querySelector('.dam').textContent = `${calculo_decametros} decâmetros (Dam)`
+
+
+  
+
 
   // let calculo_decimetros = `${distancia} decímetros (dm)`
   // let calculo_centimetros = `${distancia} centímetros (cm)`
   // let calculo_milimetros = `${distancia} milímetros (mm)`
   
 }
+
