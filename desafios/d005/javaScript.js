c03fa6890de491fddd2cf0176e6dc6ba481d26b8
jhonatasv <jhonatasvieira42@gmail.com>
2022-03-23 13:39:42 -0300

    criar d005_quilometros

--- desafios/d005/javaScript.js
+++ desafios/d005/javaScript.js
@@ -2,8 +2,20 @@ function clicou() {
   let distancia = Number(prompt('Digite uma distância em metro (m)'))
   
   let nome = document.querySelector('#texto')
-   
+  
   let texto = nome.textContent = `A distância de ${distancia} metros, corresponde a...`
   
-    
+  //calculando a distancia por (Km)
+  
+  let calculo_quilometros = `${distancia / 1000}`
+  let arredonda = calculo_quilometros.substring(0, 5) // O método substr()retorna uma parte da string O especificado no índice.
+                                                      
+  document.querySelector('.km').textContent = `${arredonda} quilômetros(Km)`
+
+  // let calculo_hectometros = `${distancia} hectômetros (Hm)`
+  // let calculo_decametros = `${distancia} decâmetros (Dam)`
+  // let calculo_decimetros = `${distancia} decímetros (dm)`
+  // let calculo_centimetros = `${distancia} centímetros (cm)`
+  // let calculo_milimetros = `${distancia} milímetros (mm)`
+  
 }
