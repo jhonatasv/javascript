0cf17a29158a8351a0ba9cdf1ae2d098c1fde239
jhonatasv <jhonatasvieira42@gmail.com>
2022-03-23 22:54:07 -0300

    feito desafio ex005

--- desafios/d005/javaScript.js
+++ desafios/d005/javaScript.js
@@ -5,31 +5,34 @@ function clicou() {
 
   let novo_Texto = paragraph.textContent = `A distância de ${distancia} metros, corresponde a...`
 
-  //calculando a quilometros (Km)
 
-  let calculo_quilometros = `${distancia / 1000}`
-  let arredonda_km = calculo_quilometros.substring(0, 5) // O método substr()retorna uma parte da string O especificado no índice.
+  let calculo_quilometros = distancia / 1000
+  let arredonda_km = calculo_quilometros.toLocaleString() // O método substr()retorna uma parte da string O especificado no índice.
   document.querySelector('.km').textContent = `${arredonda_km} quilômetros(Km)`
 
-  //calculando a hectômetros (Hm)
 
-  let calculo_hectometros = Number(`${distancia / 100}`)
-  let arredonda_Hm = calculo_hectometros.toFixed(3)      //O método toFixed() retorna uma string correspondente ao número usando, arredondando o número para a quantidade de casas decimais especificadas.
+  let calculo_hectometros = distancia / 100
+  let arredonda_Hm = calculo_hectometros.toLocaleString()     //O método toFixed() retorna uma string correspondente ao número usando, arredondando o número para a quantidade de casas decimais especificadas.
   document.querySelector('.hm').textContent = `${arredonda_Hm} hectômetros (Hm)`
 
-  //calculando O decâmetros (Dam)
-
-  let calculo_decametros = `${distancia * 10}`
-  let arredonda_Dam = calculo_decametros
+ 
+  let calculo_decametros = distancia / 10
   document.querySelector('.dam').textContent = `${calculo_decametros} decâmetros (Dam)`
 
 
+  let calculo_decimetros = distancia * 10
+  let arredonda_Dm = calculo_decimetros.toLocaleString()  //O método toLocaleString() retorna uma string com uma representação sensível a linguagem deste número.
+  document.querySelector('.dm').textContent =`${arredonda_Dm} decímetros (dm)`
+
   
+  let calculo_centimetros = distancia * 10000
+  //  let arredonda_Cm = calculo_centimetros
+   let retorna = calculo_centimetros.toLocaleString()
+  document.querySelector('.cm').textContent = `${retorna} centímetros (cm)`
 
-
-  // let calculo_decimetros = `${distancia} decímetros (dm)`
-  // let calculo_centimetros = `${distancia} centímetros (cm)`
-  // let calculo_milimetros = `${distancia} milímetros (mm)`
+  let calculo_milimetros = distancia * 1000
+  let arredonda_Mm = calculo_milimetros.toLocaleString()
+  document.querySelector('.mm').textContent = `${arredonda_Mm} milímetros (mm)`
   
 }
 
