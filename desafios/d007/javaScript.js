0ee2b17714ce0f34634f511c87de8d333c9f16cd
jhonatasv <jhonatasvieira42@gmail.com>
2022-03-24 19:06:00 -0300

    criar desafio 007

---
+++ desafios/d007/javaScript.js
@@ -0,0 +1,6 @@
+let dollarQuote = Number(prompt('Antes de mais nada, Quanto está a cotação do Dólar agora?'))
+
+function converter() {
+    let wallet = Number(prompt('Quantos R$ você tem na Carteira?'))
+    document.querySelector('.result') = `opa`
+}
