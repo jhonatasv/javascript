74d05db66054884045965b195ff69a501f5ef3e8
jhonatasv <jhonatasvieira42@gmail.com>
2022-03-24 14:51:39 -0300

    criar conversor de temperaturas

---
+++ desafios/d006/javaScript.js
@@ -0,0 +1,6 @@
+function clicou () {
+ let temperatura = Number(prompt('Digite uma temperatura em °C (Celsius)'))
+
+ document.querySelector('.temperatura_c').textContent = `${temperatura}  corresponde a...`
+
+}
