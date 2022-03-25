d9ed42dbaf4d6da36a305e80ef4c9e30ee95399e
jhonatasv <jhonatasvieira42@gmail.com>
2022-03-25 19:20:48 -0300

    document.

--- desafios/d008/javascript.js
+++ desafios/d008/javascript.js
@@ -1,7 +1,10 @@
 function desconto() {
     let product = prompt('Qual é o produto que você está comprando?')
     let price = Number(prompt(`Qual é o preço de ${product}?`))
-    let productDiscount = price
-    document.querySelector('#desconto').innerHTML = `<h2>Calculando desconto de 10% para ${product}</h2>`
+    // let productDiscount = price
+    document.querySelector('#calculando').innerHTML = `<h2>Calculando desconto de 10% para ${product}</h2>`
+    document.querySelector('.valor').textContent = `O preço original era R$ ${price}.`
+    document.querySelector('.desconto').textContent = `Você acaba de ganhar R$() de desconto (-10%).`
+    document.querySelector('.preco').textContent = `No fim, você vai pagar R$() no produto ${product}.`
 
 }
