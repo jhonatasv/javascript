7ae0fce99f07f2f3daaee862717619721e4beaf6
jhonatasv <jhonatasvieira42@gmail.com>
2022-03-29 20:10:33 -0300

    document

--- desafios/d010/javascript.js
+++ desafios/d010/javascript.js
@@ -1,3 +1,10 @@
 function calcular() {
-    
+    let a = Number(prompt('Qual é valor de a?'))
+    let b = Number(prompt('Qual é o valor de b?'))
+    let c = Number(prompt('Qual é o valor de c?'))
+
+    document.querySelector('#bhaskara').innerHTML = '<h2>Resolvendo Bhaskara</h2>'
+    document.querySelector('.equacao').innerHTML = `A equaçâo atual é <strong><sup>2</sup> + 6x + 2 = 0</strong>`
+    document.querySelector('.calculo').innerHTML = `O cáculo realizando será <strong> &#916 = 6<sup>2</sup> - 4 . 3 . 2</strong>`
+    document.querySelector('.valor').innerHTML =  `O valor calculando foi <strong><mark>&#916 = 12</mark></strong>`
 }
