125040499058e0709b098f953be9df6b152067c9
jhonatasv <jhonatasvieira42@gmail.com>
2022-03-30 13:45:28 -0300

    calculor de delta

--- desafios/d010/javascript.js
+++ desafios/d010/javascript.js
@@ -3,9 +3,10 @@ function calcular() {
     let b = parseInt(prompt('Qual é o valor de b?'))
     let c = parseInt(prompt('Qual é o valor de c?'))
 
-
+    let delta = (b * b) - 4 * a * c
+    
     document.querySelector('#bhaskara').innerHTML = '<h2>Resolvendo Bhaskara</h2>'
-    document.querySelector('.equacao').innerHTML = `A equaçâo atual é <strong><sup>2</sup> + 6x + 2 = 0</strong>`
+    document.querySelector('.equacao').innerHTML = `A equaçâo atual é <strong>${a}x<sup>2</sup> + ${b}x + ${c} = 0</strong>`
     document.querySelector('.calculo').innerHTML = `O cáculo realizando será <strong> &#916 = 6<sup>2</sup> - 4 . 3 . 2</strong>`
-    document.querySelector('.valor').innerHTML =  `O valor calculando foi <strong><mark>&#916 = 12</mark></strong>`
+    document.querySelector('.valor').innerHTML =  `O valor calculando foi <strong><mark>&#916 = ${delta}</mark></strong>`
 }
