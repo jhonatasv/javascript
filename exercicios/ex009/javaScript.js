69a6b98b58ea0ec4dad3cc96d5e1bb916e69fa15
jhonatasv <jhonatasvieira42@gmail.com>
2022-03-31 18:09:15 -0300

    ajuste

--- exercicios/ex009/javaScript.js
+++ exercicios/ex009/javaScript.js
@@ -15,8 +15,7 @@
 let star = 0
 
 function contar() {
-   cliques = star ++
-   
+    cliques = star ++
     document.querySelector('#contando').innerHTML = `O contador está com <mark>${star}</mark> cliques.`
        
 }
@@ -25,6 +24,5 @@ function contar() {
 function zerar() {
     star = 0
     document.querySelector('#contando').textContent = ''
-   
     document.querySelector('#contando').textContent = `você clicou ${cliques + 1}`
 }
