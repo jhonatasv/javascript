867e12aae9a4904e24ba0ace6b5d13e818928011
jhonatasv <jhonatasvieira42@gmail.com>
2022-03-31 18:07:50 -0300

    Contador de Cliques on

--- exercicios/ex009/javaScript.js
+++ exercicios/ex009/javaScript.js
@@ -1,12 +1,30 @@
+// let star = 0
+
+// function contar() {
+//     star ++
+//     document.querySelector('#contando').innerHTML = `O contador está com <mark>${star}</mark> cliques.`
+// }
+
+
+// function zerar() {
+//     star = 0
+//     document.getElementById('contando').textContent = ''
+// }
+
+
 let star = 0
 
 function contar() {
-    star ++
+   cliques = star ++
+   
     document.querySelector('#contando').innerHTML = `O contador está com <mark>${star}</mark> cliques.`
+       
 }
 
 
 function zerar() {
     star = 0
-    document.getElementById('contando').textContent = ''
+    document.querySelector('#contando').textContent = ''
+   
+    document.querySelector('#contando').textContent = `você clicou ${cliques + 1}`
 }
