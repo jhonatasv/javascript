98ee6a581e35584a0d7eb9c51475a2d73fcf36da
jhonatasv <jhonatasvieira42@gmail.com>
2022-03-31 18:11:42 -0300

    remoção de espaço

--- exercicios/ex009/javaScript.js
+++ exercicios/ex009/javaScript.js
@@ -16,10 +16,8 @@ let star = 0
 
 function contar() {
     cliques = star ++
-    document.querySelector('#contando').innerHTML = `O contador está com <mark>${star}</mark> cliques.`
-       
+    document.querySelector('#contando').innerHTML = `O contador está com <mark>${star}</mark> cliques.`      
 }
-
 
 function zerar() {
     star = 0
