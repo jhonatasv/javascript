6107a1d4f89a4629fb4bcb1802d648bb9bfae746
jhonatasv <jhonatasvieira42@gmail.com>
2022-04-04 17:22:49 -0300

    média do aluno v2.0 on

--- exercicios/ex011/javascript.js
+++ exercicios/ex011/javascript.js
@@ -2,9 +2,13 @@ function calcular() {
     let nome = prompt('Qual é o seu nome?')
     let nota1 = Number(prompt('Qual é a sua primeira nota?'))
     let nota2 = Number(prompt(`Além de ${nota1} qual foi a outra de ${nome}`))
+    
+    let media = (nota1 + nota2) / 2
+    let resultado = media >= 6 ? 'Meus Parabéns!' : 'Estude um pouco mais!'
 
    document.querySelector('#profile').innerHTML = `Calculando a média final de <mark>${nome}</mark>`
    document.querySelector('.grades').innerHTML = `As notas obtidas foram <mark>${nota1} e ${nota2}</mark>`
-   document.querySelector('.average').innerHTML = `A média final será <mark>(?)</mark>`
-   document.querySelector('.message').innerHTML = `A mensagem que termos é:<span class="teste">testando</span>`
+   document.querySelector('.average').innerHTML = `A média final será <mark>${media}</mark>`
+   document.querySelector('.message').innerHTML = `A mensagem que termos é: <span class="resultado">${resultado}</span>`
+ 
 }
