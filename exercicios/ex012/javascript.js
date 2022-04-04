dd05ca688eb39caa0f055cad4723379d4376a994
jhonatasv <jhonatasvieira42@gmail.com>
2022-04-04 19:31:49 -0300

    docuemnt

---
+++ exercicios/ex012/javascript.js
@@ -0,0 +1,5 @@
+function star () {
+    let numero = Number(prompt('Digite um número:'))
+    // let resultado = numero <  ? 'impar' : 'par'
+    document.querySelector('#numero').innerHTML = `O numero ${numero} que foi digitado é ${resultado}`
+}
