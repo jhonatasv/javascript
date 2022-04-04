b1abec322719980ec43ce151f1b823773fb79a25
jhonatasv <jhonatasvieira42@gmail.com>
2022-04-04 10:12:58 -0300

    ajuste em javascript

--- exercicios/ex009/javaScript.js
+++ exercicios/ex009/javaScript.js
@@ -1,11 +1,11 @@
 let star = 0
 
 function contar() {
-    cliques = star ++
+    cliques = ++ star
     document.querySelector('#contando').innerHTML = `O contador está com <mark>${star}</mark> cliques.`      
 }
 
 function zerar() {
     star = 0
-    document.querySelector('#contando').innerHTML = `você clicou ${cliques + 1} vezes &#x1F928`
+    document.querySelector('#contando').innerHTML = `você clicou ${cliques} vezes &#x1F928`
 }
