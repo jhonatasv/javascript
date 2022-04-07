10ec2a4cbd21f2c9898c20798d83b9af181f5237
jhonatasv <jhonatasvieira42@gmail.com>
2022-04-07 16:22:38 -0300

    Revert "correção d007"
    This reverts commit e156a669e1938d349c2190c5b42da530ac5ecf40.

--- desafios/d007/javaScript.js
+++ desafios/d007/javaScript.js
@@ -1,8 +1,9 @@
  let dollarQuote = Number(prompt('Antes de mais nada, Quanto está a cotação do Dólar agora?'))
 
 function converter() {
-    let wallet = parseFloat(prompt('Quantos R$ você tem na Carteira?'))
+    let wallet = parseFloat(prompt('Quantos R$ você tem na Carteira?')).toFixed(2)
     let dollarConverter = wallet / dollarQuote
     let dollarValue = dollarConverter.toFixed(2)
+    // document.querySelector('#valor').textContent = `Você tem ${wallet} R$ em reais. que em dolares é ${dollarValue}`
     document.querySelector('#valor').innerHTML = `<p>Você tem (${wallet} R&#36) em reais. que em dolares é (${dollarValue} US&#36)</p>`
 }
