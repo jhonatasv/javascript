001d33461601f727f6aebd2ec498dfc27923f03a
jhonatasv <jhonatasvieira42@gmail.com>
2022-04-08 19:42:57 -0300

    ex009 condições

---
+++ aula11/ex009.js
@@ -0,0 +1,15 @@
+// let país = 'EUA'
+// console.log(`Vivendo no ${país}`)
+// if (país == 'Brasil') {
+//     console.log('Você é Brasileiro')
+// } else {
+//     console.log('Você é Estrangeiro')
+// }
+
+let país = 'Brasil'
+console.log(`Vivendo no ${país}`)
+if (país != 'Brasil') {
+    console.log('Você é Estrangeiro')
+} else {
+    console.log('Você é Brasileiro')
+}
