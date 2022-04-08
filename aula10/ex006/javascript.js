71cc398f943a319db3eeeaf639987dd2cbb0bbcc
jhonatasv <jhonatasvieira42@gmail.com>
2022-04-08 12:48:16 -0300

    segunda forma de fazer

--- aula10/ex006/javascript.js
+++ aula10/ex006/javascript.js
@@ -1,4 +1,3 @@
-
 // let area = document.querySelector('#area')
 
 area.addEventListener('click', clicar)
@@ -6,7 +5,7 @@ area.addEventListener('mouseenter', entrar)
 area.addEventListener('mouseout', saiu)
 
 function clicar() {
-   area.innerText = 'clicou'  
+   area.innerText = 'clicou'
    area.style.background = 'black' // se caso não sejá atribuído o (const, let ou var) o escopo ficar Global
 }
 
@@ -18,3 +17,19 @@ function saiu() {
     area.innerText = 'saiu'
     area.style.background = 'green'
 }
+
+// segunda forma de fazer
+
+// area.addEventListener('click', function clicar(){
+//     area.innerText = 'clicou'
+//     area.style.background = 'black'
+// })
+
+// area.addEventListener('mouseenter', function entrar(){
+//     area.innerText = 'entrou'
+// })
+
+// area.addEventListener('mouseout', function saiu(){
+//     area.innerText = 'saiu'
+//     area.style.background = 'green'
+// })
