29dd9690d5afb360a5f65e484c2f0d262a9d330a
jhonatasv <jhonatasvieira42@gmail.com>
2022-04-12 15:25:16 -0300

    par ou ímpar on

--- exercicios/ex012/javascript.js
+++ exercicios/ex012/javascript.js
@@ -1,6 +1,11 @@
 function star () {
     let numero = Number(prompt('Digite um número:'))
-    // let resultado = numero <  ? 'impar' : 'par'
+    let result = document.querySelector('#numero')
+   
+    if (numero % 2 == 0) {
+        result.innerHTML = `O número ${numero} que foi digitado é <strong>PAR</strong>`
+    } else {
+        result.innerHTML = `O número ${numero} que foi digitado é <strong>ÍMPAR!</strong>`
+    }
 
-    document.querySelector('#numero').innerHTML = `O numero ${numero} que foi digitado é ${resultado}`
 }
