8ec64bebafe52776c433a7f7a804fc9d7d17076b
jhonatasv <jhonatasvieira42@gmail.com>
2022-04-12 20:50:38 -0300

    new Date

--- aula12/ex012.js
+++ aula12/ex012.js
@@ -1,3 +1,23 @@
+let agoras = new Date() //
+
+let horas = agoras.getHours()
+console.log(`Agora são exatamente ${horas} horas`)
+
+if(horas < 12) {  // a baixo de 12 é Bom dia 
+    console.log('Bom dia')
+} else if (horas <= 18) {  //  entre 12 e 18 é Boa tarde,
+    console.log('Boa tarde')
+} else if (horas <= 23){      // entre 18 e 23 é Boa noite
+    console.log('Boa noite')
+} else {                       //acima de 23 Madrugada
+    console.log('Boa madrugada') 
+}
+
+
+
+
+
+
 // let horas = 24
 // console.log(`Agora são exatamente ${horas} horas`)
 
@@ -10,16 +30,3 @@
 // } else {                       //acima de 23 Madrugada
 //     console.log('Boa madrugada') 
 // }
-
-let horas = 13
-console.log(`Agora são exatamente ${horas} horas`)
-
-if(horas < 12) {  // a baixo de 12 é Bom dia 
-    console.log('Bom dia')
-} else if (horas <= 18) {  //  entre 12 e 18 é Boa tarde,
-    console.log('Boa tarde')
-} else if (horas <= 23){      // entre 18 e 23 é Boa noite
-    console.log('Boa noite')
-} else {                       //acima de 23 Madrugada
-    console.log('Boa madrugada') 
-}
