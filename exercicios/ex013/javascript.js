33f561d03ca7916fed331ce53d2207e4157a6e3e
jhonatasv <jhonatasvieira42@gmail.com>
2022-04-12 16:47:38 -0300

    qual é o maior numero on

--- exercicios/ex013/javascript.js
+++ exercicios/ex013/javascript.js
@@ -1,13 +1,28 @@
+
 function star () {
     let numero1 = Number(prompt('Digite um número:'))
     let numero2 = Number(prompt('Digite um número:'))
-    
-   let maior = Math.max(numero1, numero2)
-   let menor = Math.min(numero1, numero2)
+    let result = document.querySelector('#resultado')
 
+    let maior = Math.max(numero1, numero2)
 
-    let teste = numero1 >= numero2 == menor ? `ambos são iguais` : `o maior valor é  ${maior}`
+    if (numero1 != numero2) {
+        result.innerHTML = `Analisando os valores <mark>${numero1}</mark> e  <mark>${numero2}</mark>, maior valor é <strong>${maior}</strong>`
 
-    
-    document.querySelector('#numero').innerHTML = `Analisando os valores ${numero1} e ${numero2}, <strong>${teste}<strong>`
+    } else {
+        result.innerHTML = `Analisando os valores <mark>${numero1}</mark> e  <mark>${numero2}</mark>, ambos são <strong>IGUAIS</strong>`
+    }
 }
+        
+            
+// function star () {
+//     let numero1 = Number(prompt('Digite um número:'))
+//     let numero2 = Number(prompt('Digite um número:'))
+
+//     let maior = Math.max(numero1, numero2)
+
+//     let teste = numero1 != numero2 ? `o maior valor é  ${maior}` : `ambos são iguais`
+
+//     document.querySelector('#resultado').innerHTML = `Analisando os valores ${numero1} e ${numero2}, <strong>${teste}<strong>`
+// }
+           
