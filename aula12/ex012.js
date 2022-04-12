161a137dff03d611bdd4d08bafc96a76a1e620c5
jhonatasv <jhonatasvieira42@gmail.com>
2022-04-12 20:42:54 -0300

    criar ex012

---
+++ aula12/ex012.js
@@ -0,0 +1,25 @@
+// let horas = 24
+// console.log(`Agora são exatamente ${horas} horas`)
+
+// if(horas < 12) {  // a baixo de 12 é Bom dia 
+//     console.log('Bom dia')
+// } else if (horas <= 17) {  //  entre 12 e 17 é Boa tarde,
+//     console.log('Boa tarde')
+// } else if (horas <= 23){      // entre 18 e 23 é Boa noite
+//     console.log('Boa noite')
+// } else {                       //acima de 23 Madrugada
+//     console.log('Boa madrugada') 
+// }
+
+let horas = 13
+console.log(`Agora são exatamente ${horas} horas`)
+
+if(horas < 12) {  // a baixo de 12 é Bom dia 
+    console.log('Bom dia')
+} else if (horas <= 18) {  //  entre 12 e 18 é Boa tarde,
+    console.log('Boa tarde')
+} else if (horas <= 23){      // entre 18 e 23 é Boa noite
+    console.log('Boa noite')
+} else {                       //acima de 23 Madrugada
+    console.log('Boa madrugada') 
+}
