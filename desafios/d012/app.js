071322ac031810e4ca77a72480e6ba5d72ff5c23
jhonatasv <jhonatasvieira42@gmail.com>
2022-04-20 21:36:52 -0300

    criar js d012

---
+++ desafios/d012/app.js
@@ -0,0 +1,7 @@
+btn.addEventListener('click', verificar())
+
+function verificar () {
+    let previousPrice = prompt('Qual era o preço anterior do produto?')
+    let currentPrice = prompt('Qual é o preço atual do produto?')
+    let printOut = document.querySelector('#result')
+}
