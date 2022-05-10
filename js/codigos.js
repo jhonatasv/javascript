86722f188fa0d33cc2939fffc87315fc439f1ce8
jhonatasv <jhonatasvieira42@gmail.com>
2022-05-09 23:06:46 -0300

    js. 2

--- js/codigos.js
+++ js/codigos.js
@@ -1,19 +1,45 @@
+//Objetos
+
 const person = {
     firsName: 'jhonatas',
     lastName: 'Vieira',
     age: '22',
-    hobbies: ['Jogar', 'Ler', 'Assitir filmes']
+    hobbies: ['Jogar', 'Ler', 'Assitir filmes'],
+    dog: {
+        name: 'Apollo',
+        age: 4
+    }
 }
 
+
+//Primeira Forma de Fazer
 
 // const firsName = person.firsName
 // const lastName = person.lastName
 // const age = person.age
 // const hobbies = person.hobbies[2]
 
-const {firsName: primeiroNome, lastName, age, hobbies} = person
+
+//Segunda Forma de Fazer
+
+//destructuring
+const {
+    firsName: primeiroNome,
+    lastName, 
+    age, 
+    hobbies, 
+    dog: { name: dogName}
+} = person
 
 console.log(primeiroNome)
 console.log(lastName)
 console.log(age)
 console.log(hobbies)
+
+
+//adicionando nova Propriedade
+//person.dog = 'Apollo'
+
+
+console.log(person.dog.name)
+console.log(dogName)
