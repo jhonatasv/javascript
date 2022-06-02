c0a8cde46b4f1caf8bfb72b833d4040c360f4a93
jhonatasv <jhonatasvieira42@gmail.com>
2022-06-02 20:00:02 -0300

    ex16 on

--- aulas/aula14ex/ex16/script.js
+++ aulas/aula14ex/ex16/script.js
@@ -29,12 +29,14 @@ btn.addEventListener('click', () => {
     } else {
         res.innerHTML = `<p>Contado:</p>`
         
+        
 
-        while (numberStar <= numberEnd) {
-            // res.innerHTML = `${numberStar} &#128073`
-            // res.innerHTML += `${numberStar } &#128073`
-            // numberStar ++
-        }
+         while(numberStar <= numberEnd) {
+            
+            res.innerHTML += `<mark>${numberStar} </mark> &#128073`
+           
+            numberStar = numberStar + numberPasso
+         }
         res.innerHTML += `&#127937`
 
     }
