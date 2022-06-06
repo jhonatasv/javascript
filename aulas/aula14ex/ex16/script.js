02015268f04e565d0cccb0f8b594dcef6cd1d4f8
jhonatasv <jhonatasvieira42@gmail.com>
2022-06-06 19:35:09 -0300

    correção de bug

--- aulas/aula14ex/ex16/script.js
+++ aulas/aula14ex/ex16/script.js
@@ -5,13 +5,79 @@ let res = document.querySelector('#res')
 const btn = document.querySelector('#contar')
 
 
+// btn.addEventListener('click', () => {
+//     let numberStar = Number(inputStart.value)
+//     let numberEnd = Number(inputEnd.value)
+//     let numberPasso = Number(inputPasso.value)
+
+//     if(inputStart.value === '') {
+//         res.innerHTML = `<p class="msg-erro">erro, por favor preencha o campo ìnicio!</p>`
+
+//     } else if(numberPasso === 0 || inputPasso.value === '') {
+//         res.innerHTML = `<p class="msg-erro">passo iválido, Considerando o Passo 1</p>`
+
+//         if(numberStar <= numberEnd) {
+
+//             setTimeout(() => {
+//                 res.innerHTML = '';
+
+//                 while(numberStar <= numberEnd) {
+//                     res.innerHTML += `${numberStar} &#128073`
+//                     numberStar = numberStar + numberPasso
+//                 }
+//                 res.innerHTML += `&#127937`
+
+//             }, 4000)
+
+//         } else if(numberStar >= numberEnd) {
+
+//             setTimeout(() => {
+//                 res.innerHTML = '';
+
+//                 while(numberStar >= numberEnd) {
+//                     res.innerHTML += `${numberStar} &#128073`
+//                     numberStar = numberStar - 1
+//                 }
+//                 res.innerHTML += `&#127937`
+
+//             }, 4000)
+
+//         }
+
+//     } else {
+//         res.innerHTML = `<p>Contado:</p>`
+
+//         if(numberStar <= numberEnd) {
+        
+//             while(numberStar <= numberEnd) {
+//                 res.innerHTML += `${numberStar} &#128073`
+//                 numberStar = numberStar + numberPasso
+//             }
+
+//         } else if(numberStar >= numberEnd) {
+        
+//             while(numberStar >= numberEnd) {
+//                 res.innerHTML += `${numberStar} &#128073`
+//                 numberStar = numberStar - numberPasso
+//             }
+
+//         }
+//         res.innerHTML += `&#127937`
+
+//     }
+    
+// })
+
 btn.addEventListener('click', () => {
     let numberStar = Number(inputStart.value)
     let numberEnd = Number(inputEnd.value)
     let numberPasso = Number(inputPasso.value)
 
     if(inputStart.value === '') {
-        res.innerHTML = `<p class="msg-erro">erro, por favor preencha o campo ìnicio!</p>`
+        res.innerHTML = `<p class="msg-erro">erro, impossível de contar!</p>`
+
+    } else if(inputEnd.value === '') {
+        res.innerHTML = `<p class="msg-erro">erro, impossível de contar!</p>`
 
     } else if(numberPasso === 0 || inputPasso.value === '') {
         res.innerHTML = `<p class="msg-erro">passo iválido, Considerando o Passo 1</p>`
@@ -23,23 +89,24 @@ btn.addEventListener('click', () => {
 
                 while(numberStar <= numberEnd) {
                     res.innerHTML += `${numberStar} &#128073`
-                    numberStar = numberStar + numberPasso
+                    numberStar ++
                 }
                 res.innerHTML += `&#127937`
 
             }, 4000)
 
-        } else if(numberStar >= numberEnd) {
 
+        } else if(numberStar >= numberEnd) {
+           
             setTimeout(() => {
                 res.innerHTML = '';
-
+        
                 while(numberStar >= numberEnd) {
                     res.innerHTML += `${numberStar} &#128073`
-                    numberStar = numberStar - 1
+                    numberStar -- 
                 }
                 res.innerHTML += `&#127937`
-
+        
             }, 4000)
 
         }
@@ -48,22 +115,23 @@ btn.addEventListener('click', () => {
         res.innerHTML = `<p>Contado:</p>`
 
         if(numberStar <= numberEnd) {
-        
+    
             while(numberStar <= numberEnd) {
                 res.innerHTML += `${numberStar} &#128073`
                 numberStar = numberStar + numberPasso
             }
-
+            
         } else if(numberStar >= numberEnd) {
-        
+
             while(numberStar >= numberEnd) {
                 res.innerHTML += `${numberStar} &#128073`
                 numberStar = numberStar - numberPasso
             }
-
+                
         }
         res.innerHTML += `&#127937`
-
+    
     }
     
 })
+
