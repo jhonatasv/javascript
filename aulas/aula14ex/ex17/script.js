9f8cee50188c609c473aee4894cbf153f73c36a0
jhonatasv <jhonatasvieira42@gmail.com>
2022-06-06 14:18:51 -0300

    verificação de input

--- aulas/aula14ex/ex17/script.js
+++ aulas/aula14ex/ex17/script.js
@@ -4,9 +4,15 @@ const res = document.querySelector('#res')
 
 btn.addEventListener('click', () => {
     let iputNumero = Number(inpuText.value) 
-    res.innerHTML = '';
 
-    for(let i = 1; i <= 10; i++) {
-        res.innerHTML += `<p>${iputNumero} x ${i} = ${iputNumero * i}</p>`
+    if(inpuText.value === '') {
+        res.innerHTML = `<p>Por favor Digite um Numero!</p>`
+    } else {
+
+        res.innerHTML = '';
+        for(let i = 1; i <= 10; i++) {
+            res.innerHTML += `<p>${iputNumero} x ${i} = ${iputNumero * i}</p>`
+        }
     }
+
 })
