a3c52d486ef30bb8d85c37bacc0fcf7b537ed2df
jhonatasv <jhonatasvieira42@gmail.com>
2022-06-07 19:09:23 -0300

    ajuste no ex27

---
+++ exercicios/Nível 3. Repetições em JavaScript/ex27/app.js
@@ -0,0 +1,17 @@
+const inpuTxt = document.querySelector('#txtn')
+const btn = document.querySelector('#tab')
+const res =document.querySelector('#res')
+
+btn.addEventListener('click', () => {
+    let num = Number(inpuTxt.value)
+
+    if(inpuTxt.value === '') {
+        res.innerHTML = `<p class="msg-erro">Por Favor, insira um Número</p>`
+    } else {
+        res.innerHTML = `<h2>Tabuada do ${num}</h2>`
+
+        for(let i = 1; i <= 10; i++) {
+            res.innerHTML += `<p>${num} x ${i} = <strong> ${num * i}</strong></p>`
+        }
+    }
+})
