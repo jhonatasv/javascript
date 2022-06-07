4cbe5fb8a671f02d1e2f4befaf0ec8d62d3d5fa0
jhonatasv <jhonatasvieira42@gmail.com>
2022-06-07 14:57:42 -0300

    lógica do (guana) em js

---
+++ aulas/aula14ex/lógica-do-guanabara/ex17/script.js
@@ -0,0 +1,20 @@
+let num = document.getElementById('txtn')
+let tab = document.getElementById('seltab')
+
+function tabuada() {
+    let n = Number(num.value)
+
+    if(num.value.length == '') {
+        alert('Por favor, Digite um Número!')
+        
+    } else {
+        let c = 1
+        
+        while(c <= 10) {
+            let teste = document.createElement('option')
+            
+        }
+       console.log(`opa`)
+    }
+}
+
