3603b9290ad1c9f30123914319f4d170a624b492
jhonatasv <jhonatasvieira42@gmail.com>
2022-06-06 21:05:31 -0300

    simplificando Auto-Atribuição

--- aulas/aula14ex/ex16/script.js
+++ aulas/aula14ex/ex16/script.js
@@ -5,69 +5,6 @@ let res = document.querySelector('#res')
 const btn = document.querySelector('#contar')
 
 
-// btn.addEventListener('click', () => {
-//     let numberStar = Number(inputStart.value)
-//     let numberEnd = Number(inputEnd.value)
-//     let numberPasso = Number(inputPasso.value)
-
-//     if(inputStart.value === '') {
-//         res.innerHTML = `<p class="msg-erro">erro, por favor preencha o campo ìnicio!</p>`
-
-//     } else if(numberPasso === 0 || inputPasso.value === '') {
-//         res.innerHTML = `<p class="msg-erro">passo iválido, Considerando o Passo 1</p>`
-
-//         if(numberStar <= numberEnd) {
-
-//             setTimeout(() => {
-//                 res.innerHTML = '';
-
-//                 while(numberStar <= numberEnd) {
-//                     res.innerHTML += `${numberStar} &#128073`
-//                     numberStar = numberStar + numberPasso
-//                 }
-//                 res.innerHTML += `&#127937`
-
-//             }, 4000)
-
-//         } else if(numberStar >= numberEnd) {
-
-//             setTimeout(() => {
-//                 res.innerHTML = '';
-
-//                 while(numberStar >= numberEnd) {
-//                     res.innerHTML += `${numberStar} &#128073`
-//                     numberStar = numberStar - 1
-//                 }
-//                 res.innerHTML += `&#127937`
-
-//             }, 4000)
-
-//         }
-
-//     } else {
-//         res.innerHTML = `<p>Contado:</p>`
-
-//         if(numberStar <= numberEnd) {
-        
-//             while(numberStar <= numberEnd) {
-//                 res.innerHTML += `${numberStar} &#128073`
-//                 numberStar = numberStar + numberPasso
-//             }
-
-//         } else if(numberStar >= numberEnd) {
-        
-//             while(numberStar >= numberEnd) {
-//                 res.innerHTML += `${numberStar} &#128073`
-//                 numberStar = numberStar - numberPasso
-//             }
-
-//         }
-//         res.innerHTML += `&#127937`
-
-//     }
-    
-// })
-
 btn.addEventListener('click', () => {
     let numberStar = Number(inputStart.value)
     let numberEnd = Number(inputEnd.value)
@@ -118,14 +55,14 @@ btn.addEventListener('click', () => {
     
             while(numberStar <= numberEnd) {
                 res.innerHTML += `${numberStar} &#128073`
-                numberStar = numberStar + numberPasso
+                numberStar += numberPasso
             }
             
         } else if(numberStar >= numberEnd) {
 
             while(numberStar >= numberEnd) {
                 res.innerHTML += `${numberStar} &#128073`
-                numberStar = numberStar - numberPasso
+                numberStar -= numberPasso
             }
                 
         }
