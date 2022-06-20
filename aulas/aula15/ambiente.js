730b701344ab0750f63e53c58084f2bb6633741e
jhonatasv <jhonatasvieira42@gmail.com>
2022-06-20 17:43:24 -0300

    add indexOf()

--- aulas/aula15/ambiente.js
+++ aulas/aula15/ambiente.js
@@ -1,7 +1,15 @@
 let num = [5, 8, 2, 9, 3]
 num.push(1) //adicionando valor no ultimo índece
 num.sort() // retornando lista ordenada
-console.log(`Nosso vetor é ${num}`)
+console.log(num)
 console.log(`o vetor tem ${num.length} posições`)
 console.log(`o primeiro valor é ${num[0]}`)
+
+let pos = num.indexOf(8)
+if(pos == -1) {
+    console.log('o numero não foi encontrado')
+} else {
+    console.log(`o valor está na posicão ${pos}`)
+}
+
 //console.table(num)
