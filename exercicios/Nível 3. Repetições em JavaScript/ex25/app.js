0fd05c17f5eef8e53c0df1a30277d1489b51e69e
jhonatasv <jhonatasvieira42@gmail.com>
2022-06-23 18:12:06 -0300

    removendo variável (i) no ex25

--- exercicios/Nível 3. Repetições em JavaScript/ex25/app.js
+++ exercicios/Nível 3. Repetições em JavaScript/ex25/app.js
@@ -5,23 +5,22 @@ const res = document.querySelector('#res')
 btn.addEventListener('click', () => {
    let inputValue = parseInt(inpuNUmber.value)
 
-   let i = 0
 
-    if(inputValue < i) {
-       res.innerHTML = `<p class="msg-error">[Error] insira um numero positivo</p>`
+   if(inputValue < 0) {
+      res.innerHTML = `<p class="msg-error">[Error] insira um numero positivo</p>`
    
-    } else if(inpuNUmber.value.length === 0) {
-       res.innerHTML = `<p class="msg-error"> Ops.. Insira um número</p>`
+   } else if(inpuNUmber.value.length === 0) {
+      res.innerHTML = `<p class="msg-error"> Ops.. Insira um número</p>`
 
-    } else {
-       res.innerHTML = `<h2>Contando de 0 até ${inputValue}</h2>`
+   } else {
+      res.innerHTML = `<h2>Contando de 0 até ${inputValue}</h2>`
 
 
-        for(let a = 0; a <= inputValue; a++) {
-            res.innerHTML += `${a}&#128073`
-        }
-        res.innerHTML += `&#127937`
-    }
+      for(let i = 0; i <= inputValue; i++) {
+         res.innerHTML += `${i}&#128073`
+      }
+      res.innerHTML += `&#127937`
+   }
     
 
 })
