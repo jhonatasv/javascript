ac0ac8a18d848780aa3ddb5b6434876bc726075e
jhonatasv <jhonatasvieira42@gmail.com>
2022-06-23 16:21:23 -0300

    melhoria na função do ex09

--- exercicios/Nìvel 1. JavaScript Básico/ex009/javaScript.js
+++ exercicios/Nìvel 1. JavaScript Básico/ex009/javaScript.js
@@ -1,11 +1,13 @@
 let star = 0
+let res = document.querySelector('#contando')
 
 function contar() {
-    cliques = ++ star
-    document.querySelector('#contando').innerHTML = `O contador está com <mark>${star}</mark> cliques.`      
+    star ++
+    res.innerHTML = `O contador está com <mark>${star}</mark> cliques.` 
+    return star     
 }
 
 function zerar() {
+    res.innerHTML = `você clicou ${star} vezes &#x1F928`
     star = 0
-    document.querySelector('#contando').innerHTML = `você clicou ${cliques} vezes &#x1F928`
 }
