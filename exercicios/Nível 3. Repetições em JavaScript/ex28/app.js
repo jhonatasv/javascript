9ab6baef4cc1314f25582a559d0159523651e993
jhonatasv <jhonatasvieira42@gmail.com>
2022-06-24 14:13:18 -0300

    ajuste em ex28

--- exercicios/Nível 3. Repetições em JavaScript/ex28/app.js
+++ exercicios/Nível 3. Repetições em JavaScript/ex28/app.js
@@ -5,16 +5,21 @@ const res = document.querySelector("#res")
 btn.addEventListener('click', () => {
     let inpuValue = txtInput.value
 
-    res.innerHTML = `<h2>Calculando ${inpuValue}!</h2>`
-    let fator = 1
-    
-    
-    for(let c = inpuValue; c > 1; c--) {
-        fator *= c
-        res.innerHTML += `${c} x ` 
-    }
+    if(txtInput.value == '') {
+        res.innerHTML = `<p class="msg-error">Ops...Preencha o campo </p>`
+    } else if(txtInput.value > 21) {
+        res.innerHTML = `<p class="msg-error">[Error], Preencha o campo entre (1 e 21)<p>`
+    } else {
+        res.innerHTML = `<h2>Calculando ${inpuValue}!</h2>`
+        let fator = 1
 
-    res.innerHTML += `1 = <strong>${fator.toLocaleString()}</strong>` 
+        for(let c = inpuValue; c > 1; c--) {
+            fator *= c
+            res.innerHTML += `${c} x ` 
+        }
+        res.innerHTML += `1 = <strong>${fator.toLocaleString()}</strong>` 
+    
+    }
     
   
 })
