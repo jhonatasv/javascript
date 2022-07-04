cc54e012d7e1d4ad555c47e840dd822b48ca437e
jhonatasv <jhonatasvieira42@gmail.com>
2022-07-04 12:31:11 -0300

    validando btn finalizar

--- aulas/aula16ex/script.js
+++ aulas/aula16ex/script.js
@@ -60,18 +60,28 @@ add.addEventListener('click', () => {
 
 
 finalizar.addEventListener('click', () => {
-    let tamanho = lista.length
-    let maiorValor = Math.max(...lista)
-    let menorValor = Math.min(...lista) // (...spred) operator
-    let somandoTodos = lista.reduce((total, acc) => {
-        return acc + total
-    }, 0)
-    let mediaValores =  somandoTodos / tamanho;
 
-    res.innerHTML = `<p>Ao todo, temos ${tamanho} números cadastrado.</p>`
-    res.innerHTML += `<p>O maior valor infomado foi ${maiorValor}.</p>`
-    res.innerHTML += `<p>O menor valor informado foi ${menorValor}.</p>`
-    res.innerHTML += `<p>Somando todos os valores, temos ${somandoTodos}.</p>`
-    res.innerHTML += `<p>A média dos valores digitados é ${mediaValores}.</p>`
+    if(lista.length == 0) {
+        res.innerHTML = `<p class="msg-error">Por favor, Adicione valores antes de Finalizar</p>`
+
+
+    } else {
+
+        let tamanho = lista.length
+        let maiorValor = Math.max(...lista)
+        let menorValor = Math.min(...lista) // (...spred) operator
+        let somandoTodos = lista.reduce((total, acc) => {
+            return acc + total
+        }, 0)
+        let mediaValores =  somandoTodos / tamanho;
+
+        res.innerHTML = `<p>Ao todo, temos ${tamanho} números cadastrado.</p>`
+        res.innerHTML += `<p>O maior valor infomado foi ${maiorValor}.</p>`
+        res.innerHTML += `<p>O menor valor informado foi ${menorValor}.</p>`
+        res.innerHTML += `<p>Somando todos os valores, temos ${somandoTodos}.</p>`
+        res.innerHTML += `<p>A média dos valores digitados é ${mediaValores}.</p>`
+
+    }
+
 })
 
